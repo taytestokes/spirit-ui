9f93305bb9aa48e93025aded97aa6af812c4a98f
taytestokes <taytestokes9328@gmail.com>
2024-10-15 20:27:07 -0600

    Add shared typescript config

--- packages/components/button/src/button.tsx
+++ packages/components/button/src/button.tsx
@@ -1,7 +1,5 @@
 // TODO: Configure the typscript compiler
 // TODO: Setup storybook
-import React from "react";
-
 export const Button = () => {
   return <button>Hello World! I'm a button.</button>;
 };
