9694928323d5a8e59fe9cecb3bc9a5f7ae2e8bc1
taytestokes <taytestokes9328@gmail.com>
2024-10-18 10:59:16 -0600

    Update tsconfig and implement tsup for building

---
+++ packages/components/button/src/button.stories.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./button";
+
+const meta = {
+  title: "Button",
+  component: Button,
+};
+
+export default meta;
+
+export const Primary = {
+  args: {
+    appName: "Button",
+    children: "I am a primary button.",
+  },
+};
--- packages/components/button/src/button.tsx
+++ packages/components/button/src/button.tsx
@@ -1,5 +1,13 @@
-// TODO: Configure the typscript compiler
-// TODO: Setup storybook
-export const Button = () => {
-  return <button>Hello World! I'm a button.</button>;
+import React from 'react';
+
+// import classes from './button.module.css';
+
+// TODO:
+// Finish setting up tsup for bundling with css modules
+// Start writing style system using css modules (foundation styles colors, text, spacing, etc)
+// Revisit eslint rules
+// Decide what to use for styling? Ideally css modules but not sure if supported from tsup?
+
+export const Button: React.FC = () => {
+  return <button>Hello, Button!</button>;
 };
