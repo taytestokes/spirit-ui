d99a620c60d9ad3db8156908ca06dc44a6a1cee1
taytestokes <taytestokes9328@gmail.com>
2024-11-02 12:58:44 -0600

    Updated @spirit-ui/button to v0.0.1

--- packages/components/button/tsup.config.ts
+++ packages/components/button/tsup.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   clean: true,
-  dts: true,
+  // TODO: TURN DTS BACK ON
+  dts: false,
   entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
   outDir: 'dist',
