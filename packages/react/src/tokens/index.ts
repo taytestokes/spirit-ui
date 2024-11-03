b18bd48ef3f7ffa5d9af5db144ca9247a5633b75
taytestokes <taytestokes9328@gmail.com>
2024-11-03 12:24:29 -0700

    Update colors and initialized nextjs docs app

--- packages/react/src/tokens/index.ts
+++ packages/react/src/tokens/index.ts
@@ -1,6 +1,6 @@
-import { colors } from "./color";
-import { spacings } from "./spacing";
-import { fontSizes, fontWeights } from "./typography";
+import { colors } from './color';
+import { spacings } from './spacing';
+import { fontSizes, fontWeights } from './typography';
 
 export const tokens = {
   colors,
