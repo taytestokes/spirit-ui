38003622840ca0f4dcc8dcdef98c5ad53546b6a6
taytestokes <taytestokes9328@gmail.com>
2024-11-07 12:08:02 -0700

    Bump to 0.0.7 to test

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,3 +1,10 @@
+import {} from "@spirit-ui/react";
+
 export default function Home() {
-  return <div>Spirit UI</div>;
+  return (
+    <div>
+      Spirit UI
+      <Button>Hello!</Button>
+    </div>
+  );
 }
