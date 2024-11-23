344da0817c3205d25fa87919d62629ebe6129348
taytestokes <taytestokes9328@gmail.com>
2024-11-23 13:36:48 -0700

    Update layout for docs

--- apps/docs/app/layout.css.ts
+++ apps/docs/app/layout.css.ts
@@ -1,6 +1,72 @@
-import { style } from "@vanilla-extract/css";
+import { style, createVar } from "@vanilla-extract/css";
+import { calc } from "@vanilla-extract/css-utils";
 
-export const body = style({
-  color: "white",
-  backgroundColor: "blue",
+import { theme } from "@spirit-ui/theme";
+
+const headerHeight = createVar();
+const maxContentWidth = createVar();
+const autoMargin = createVar();
+
+export const html = style({
+  vars: {
+    [headerHeight]: "64px",
+    [maxContentWidth]: "1000px",
+    [autoMargin]: "0 auto",
+  },
+  color: theme.colors.gray950,
+  fontFamily: theme.fonts.inter,
+});
+
+export const container = style({
+  display: "flex",
+  flexDirection: "column",
+  height: "100vh",
+  width: "100vw",
+});
+
+export const header = style({
+  border: `1px solid ${theme.colors.gray200}`,
+  height: headerHeight,
+});
+
+export const headerContent = style({
+  alignItems: "center",
+  display: "flex",
+  height: "100%",
+  margin: autoMargin,
+  maxWidth: maxContentWidth,
+  paddingInline: theme.space.spacingL,
+});
+
+export const main = style({
+  display: "grid",
+  gridTemplateColumns: "260px 1fr",
+  height: calc.subtract("100%", headerHeight),
+  margin: autoMargin,
+  maxWidth: maxContentWidth,
+  width: "100%",
+});
+
+export const sidebar = style({
+  height: "100%",
+  paddingInline: theme.space.spacingL,
+});
+
+export const sidebarSection = style({
+  paddingBlock: theme.space.spacingL,
+});
+
+export const sidebarSectionNav = style({
+  listStyle: "none",
+});
+
+export const sidebarSectionLink = style({
+  color: theme.colors.gray950,
+  textDecoration: "none",
+});
+
+export const content = style({
+  height: "100%",
+  overflow: "auto",
+  paddingInline: theme.space.spacingL,
 });
--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -1,4 +1,8 @@
 import type { Metadata } from "next";
+import Link from "next/link";
+
+import "@spirit-ui/theme/reset";
+import "@spirit-ui/theme/styles";
 
 import * as styles from "./layout.css";
 
@@ -13,10 +17,28 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={styles.body}>
-        <div>
-          <main>{children}</main>
+    <html className={styles.html} lang="en">
+      <body>
+        <div className={styles.container}>
+          <header className={styles.header}>
+            <div className={styles.headerContent}>
+              <h3>Spirit UI</h3>
+            </div>
+          </header>
+          <main className={styles.main}>
+            <aside className={styles.sidebar}>
+              <div className={styles.sidebarSection}>
+                <nav className={styles.sidebarSectionNav}>
+                  <li>
+                    <Link className={styles.sidebarSectionLink} href="/colors">
+                      <span>Colors</span>
+                    </Link>
+                  </li>
+                </nav>
+              </div>
+            </aside>
+            <div className={styles.content}>{children}</div>
+          </main>
         </div>
       </body>
     </html>
