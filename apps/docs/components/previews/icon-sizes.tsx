8be26bf4fa3504b2a8febc9a6f5bfc905c661046
taytestokes <taytestokes9328@gmail.com>
2025-03-05 12:12:03 -0700

    Add icon component documentation and previews

---
+++ apps/docs/components/previews/icon-sizes.tsx
@@ -0,0 +1,11 @@
+import { Box, Icon } from "@spirit-ui/design-system/components";
+
+export default function Component() {
+  return (
+    <Box display="flex" gap="x2">
+      <Icon name="SparklesIcon" size={16} />
+      <Icon name="SparklesIcon" size={20} />
+      <Icon name="SparklesIcon" size={24} />
+    </Box>
+  );
+}
---
+++ apps/docs/components/previews/icon.tsx
@@ -0,0 +1,5 @@
+import { Icon } from "@spirit-ui/design-system/components";
+
+export default function Component() {
+  return <Icon name="SparklesIcon" size={24} />;
+}
